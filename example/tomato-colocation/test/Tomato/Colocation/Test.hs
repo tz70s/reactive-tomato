a5c5c798eed40fe8507078928b6cc32f88cb8b47
Tzu-Chiao Yeh <su3g4284zo6y7@gmail.com>
2019-03-31 02:34:51 +0800

    Add quickcheck tests for colocation tests.

--- example/tomato-colocation/test/Tomato/Colocation/Test.hs
+++ example/tomato-colocation/test/Tomato/Colocation/Test.hs
@@ -11,6 +11,8 @@ import           Data.Unique
 import           System.IO.Unsafe
 import           Test.Tasty
 import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
+import           Data.Text                     as Text
 
 tests :: TestTree
 tests = testGroup
@@ -19,6 +21,17 @@ tests = testGroup
   , testCase "Insert two event and build relation" insert2
   , testCase "Insertion should be idempotent"      idempotent
   ]
+
+arbitraryE :: Gen IdEvent
+arbitraryE = do
+  let _id = unsafePerformIO newUnique
+  _location <- arbitrary
+  _speed    <- arbitrary
+  _device   <- arbitrary
+  IdEvent _id . RealWorldEvent _location _speed (Text.pack _device) . Text.pack <$> arbitrary
+
+instance Arbitrary IdEvent where
+  arbitrary = arbitraryE
 
 withId :: RealWorldEvent -> IdEvent
 withId r = unsafePerformIO $ do
@@ -30,6 +43,19 @@ event1 = withId $ RealWorldEvent (1, 1) (1, 1) "event1" "event1"
 
 event2 :: IdEvent
 event2 = withId $ RealWorldEvent (4, 5) (1, 1) "event2" "event2"
+
+prop_insert :: IdEvent -> Bool
+prop_insert evt = updateView evt newView == (CurrentView $ Map.fromList [(evt, [])])
+
+prop_insert2 :: IdEvent -> IdEvent -> Bool
+prop_insert2 evt1 evt2 =
+  (updateView evt2 . updateView evt1) newView
+    == (CurrentView $ Map.fromList [(evt1, [(evt2, High)]), (evt2, [(evt1, High)])])
+
+prop_idempotent :: IdEvent -> IdEvent -> Bool
+prop_idempotent evt1 evt2 =
+  (updateView evt2 . updateView evt2 . updateView evt1) newView
+    == (CurrentView $ Map.fromList [(evt1, [(evt2, High)]), (evt2, [(evt1, High)])])
 
 insert :: Assertion
 insert = updateView event1 newView @?= (CurrentView $ Map.fromList [(event1, [])])
